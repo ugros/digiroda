14039af80f90c7add7d046ea17490c21d6431654
Ugró Sándor <legyenwebed@gmail.com>
2018-11-25 19:04:34 +0100

    New menu: logs - open menu file with a filechooser - change DigiListeners.java to DigiMenuListener.java - change switch/case to if/else in MenuListener and use fieds of language.properties in listener expressions (like constants)

--- src/digiroda/DigIroda.java
+++ src/digiroda/DigIroda.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2018 ugros.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package digiroda;
 
 import static digiroda.DigiController.LOGGER;
@@ -16,11 +30,10 @@ import javafx.stage.Stage;
 
 
 public class DigIroda extends Application {
-static Parent root ; 
 
     @Override
     public void start(Stage stage) throws Exception {
-        root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.getIcons().add(new Image("resources/images/office.png"));
